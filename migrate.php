ec40bf0e8fc5c19c477c5862dc8db8fd48b7890d
bufan354 <bufan354@gmail.com>
2026-04-26 23:03:30 +0700

    Cleanup: Konsolidasi skema database ke schema.sql dan hapus file migrasi lama

--- migrate.php
+++
@@ -1,8 +0,0 @@
-require_once 'core/Session.php';
-Session::requireSuperAdmin();
-
-require_once 'core/Database.php';
-$db = Database::getConnection();
-$sql = file_get_contents('database/schema.sql');
-$db->exec($sql);
-echo "Migrated!\n";
